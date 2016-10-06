41adbd11ee1ba9fba3a4b42486f812e98acff1fc
Virendra.Rawat <Virendra.Rawat@GCPGXZ1.galileo.corp.lcl>
2016-10-06 13:43:18 +1100

    Browserclassupload

---
+++ src/qccAutomation/launchBrowser.java
@@ -0,0 +1,39 @@
+package qccAutomation;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class launchBrowser {
+
+	private static WebDriver driver = null;
+
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	private void setDriver(String ScriptDriver) {
+		
+		if (ScriptDriver == "chrome"){
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\eclipse\\chromedriver.exe");
+	        driver = new ChromeDriver();	
+		}
+		
+		if (ScriptDriver == "FireFox"){	
+			System.setProperty("webdriver.firefox.marionette","C:\\geckodriver\\geckodriver.exe");
+			
+			driver = new FirefoxDriver();		
+		}	
+	}
+
+	public void OpenBrowser(String autUrl) {
+		setDriver("FireFox");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(autUrl);
+		System.out.println("QCC Application launched Successfully");
+	}
+
+}
